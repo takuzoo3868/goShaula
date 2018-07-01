dac666ff200383bcead779764d83cee7edb6374f
takuzoo <sata3868.k.serpentis@gmail.com>
2018-07-01 09:32:11 +0900

    :+1: Socket通信の実装

--- client.go
+++ client.go
@@ -1,1 +1,54 @@
-package goShaula
+package main
+
+import (
+	"fmt"
+	"net"
+	"time"
+	"log"
+	"strconv"
+)
+
+var (
+	serverIP   = "127.0.0.1"
+	serverPort = "8080"
+	myIP       = "127.0.0.1"
+	myPort     = "8081"
+
+	message = "this is a sample message."
+)
+
+func main() {
+
+	// server情報
+	tcpAddr, err := net.ResolveTCPAddr("tcp", serverIP+":"+serverPort)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// client情報
+	myAddr := new(net.TCPAddr)
+	myAddr.IP = net.ParseIP(myIP)
+	myAddr.Port, _ = strconv.Atoi(myPort)
+
+	// serverへ接続
+	conn, err := net.DialTCP("tcp", myAddr, tcpAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	// messageの送信
+	conn.SetWriteDeadline(time.Now().Add(10 * time.Second))
+	conn.Write([]byte(message))
+
+	// serverからの応答
+	readBuf := make([]byte, 1024)
+	conn.SetReadDeadline(time.Now().Add(10 * time.Second))
+	readlen, err := conn.Read(readBuf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("server: " + string(readBuf[:readlen]))
+
+}
--- server.go
+++ server.go
@@ -1,1 +1,53 @@
-package goShaula
+package main
+
+import (
+	"fmt"
+	"net"
+	"time"
+	"log"
+)
+
+var (
+	port = "8080" // listen port
+)
+
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+	fmt.Println("client accept!")
+
+	// メッセージ受信
+	conn.SetReadDeadline(time.Now().Add(10 * time.Second))
+	messageBuf := make([]byte, 1024)
+	massageLen, err := conn.Read(messageBuf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	message := string(messageBuf[:massageLen])
+
+	// メッセージ応答
+	conn.SetWriteDeadline(time.Now().Add(10 * time.Second))
+	conn.Write([]byte(message))
+}
+
+func main() {
+	// port監視
+	tcpAddr, err := net.ResolveTCPAddr("tcp", ":"+port) // end point of L4
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 新規接続があればgoroutine(非同期レスポンス)起動
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		go handleClient(conn)
+	}
+}
